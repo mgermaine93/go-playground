6c854f96eaf2e81de567b41dc43670c5141d49ac
mgermaine93 <mgermaine93@gmail.com>
2021-01-16 22:55:38 -0500

    Initial commit.

---
+++ 4-variables-values-and-type/hello-world.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+// Entry point of Go programs will ALWAYS be "package main" and "func main()"
+// Once the program exits "func main()", the program is DONE
